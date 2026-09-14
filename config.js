9b2c0ae44353ccd627f697383b4336dd0411b882
kxro-bbx <kuroscloudsmurf@gmail.com>
2026-09-14 17:53:50 +0200

    Set SHEET_CSV_URL to published Google Sheets URL

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Beispiel: Google Sheet -> Datei -> Freigeben -> Im Web veröffentlichen -> Tabellenblatt1 -> CSV.
 // Wenn die URL leer bleibt, nutzt die Seite die mitgelieferten Snapshot-Daten aus data/blades.json.
 window.BBX_CONFIG = {
-  SHEET_CSV_URL: "",
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwBgnD3ez3e15KJmn_3cgVMn1ejEWEknmvp1UmBZIUfMOciDxCIv_xSaCic3SlibMcRGSe4g0deUHr/pub?gid=0&single=true&output=csv",
   SHEET_NAME: "Tabellenblatt1",
   DEFAULT_BUDGET: 18,
   MAX_BUDGET: 28,
